c57c62ddb65c182822ad13cced889772d9f1b8c8
PrajithG391 <kuttanthampuran3669@gmail.com>
2025-09-24 13:46:20 +0530

    auto commit - $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')

--- student_mgmt/accounts/views.py
+++ student_mgmt/accounts/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import StudentCreationForm
 # Create your views here.
