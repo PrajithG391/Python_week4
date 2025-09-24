446e1faeeec67f297ab8529d7ee0910c68245461
PrajithG391 <kuttanthampuran3669@gmail.com>
2025-09-24 10:52:39 +0530

    auto commit - $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')

--- student_mgmt/student_mgmt/urls.py
+++ student_mgmt/student_mgmt/urls.py
@@ -20,4 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('day1.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings. MRDIA_ROOT)
