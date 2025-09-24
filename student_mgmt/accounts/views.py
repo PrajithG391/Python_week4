2653773387cebe547847538070f4ebf11ff3a440
PrajithG391 <kuttanthampuran3669@gmail.com>
2025-09-24 13:46:59 +0530

    auto commit - $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')

--- student_mgmt/accounts/views.py
+++ student_mgmt/accounts/views.py
@@ -3,3 +3,14 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import StudentCreationForm
 # Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = StudentCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile') # Redirect to the profile page after registration
+    else:
+        form = StudentCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
