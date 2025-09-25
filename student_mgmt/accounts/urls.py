00ebf639e93c541b6fd63ab15340ae91bfc5dc77
PrajithG391 <kuttanthampuran3669@gmail.com>
2025-09-25 12:07:22 +0530

    auto commit - $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')

---
+++ student_mgmt/accounts/urls.py
@@ -0,0 +1,14 @@
+# accounts/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile_view, name='profile'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('dashboard/admin/', views.admin_dashboard_view, name='admin_dashboard'),
+    path('dashboard/student/', views.student_dashboard_view, name='student_dashboard'),
+]
